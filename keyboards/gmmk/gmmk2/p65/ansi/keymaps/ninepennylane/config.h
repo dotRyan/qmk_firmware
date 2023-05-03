29cca27aeb9e8b820243808d1a72ca2d62db4b3f
Ryan Robinson <dotryanrobinson@gmail.com>
2023-05-03 18:04:16 -0400

    Added Leighannes keyboard

---
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/ninepennylane/config.h
@@ -0,0 +1,4 @@
+#define RGB_DISABLE_TIMEOUT 300000  // 5 minutes (5 * 60 * 1000ms)
+// #define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+// #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR  // Sets the default mode, if none has been set
+// #define RGB_MATRIX_STARTUP_HUE 85 // Sets the default hue value, if none has been set
