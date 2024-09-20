8b1de258425061c9a818b0e9e878541b5f4a7161
Ryan Robinson <dotryanrobinson@gmail.com>
2024-09-20 13:11:56 -0400

    Updates

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/robotnoisez/config.h
@@ -0,0 +1,3 @@
+#define RGB_DISABLE_TIMEOUT 300000  // 5 minutes (5 * 60 * 1000ms)
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CUSTOM_robotnoisez_gradient // Sets the default mode, if none has been set
--- keyboards/gmmk/pro/rev1/ansi/keymaps/robotnoisez/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/robotnoisez/keymap.c
@@ -15,13 +15,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Del           Rotary(Mute)
-//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Home
+//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12      Prt           Rotary(Mute)
+//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)      BackSpc           Del
 //      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
 //      Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
 //      Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
@@ -40,23 +38,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
+
     [1] = LAYOUT(
-        RGB_TOG, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_INS,           _______,
-        _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_SPI, RGB_HUI, RGB_SAI, _______, _______, KC_DEL,           KC_SCRL,
+        RGB_TOG, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_SCRL,           _______,
+        _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAD, RGB_VAI, _______,          KC_INS,
         _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
         _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,          _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, KC_APP , _______, _______, KC_SCRL,          _______, KC_PGUP, _______,
-        _______, GU_TOGG, _______,                            KC_PSCR,                            _______, _______, _______, KC_HOME, KC_PGDN, KC_END
+        _______,          _______, _______, _______, _______, _______, _______, KC_APP , _______, _______, KC_SCRL,          _______, KC_PGUP, KC_HOME,
+        _______, _______, _______,                            _______,                            _______, _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
 
-
+// KC_INS
 };
 // clang-format on
 
@@ -86,10 +85,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #ifdef RGB_MATRIX_ENABLE
 static void set_rgb_caps_leds(void);
 static uint8_t get_current_brightness(void);
-static uint8_t get_flipped_hue(void);
 static void set_rgb_scroll_lock_leds(void);
-static void set_no_gui_leds(void);
-
 
 // RGB led number layout, function of the key
 
@@ -131,17 +127,6 @@ uint8_t SCROLL_LOCK_LEDS[] = {
     88, //led 18
 };
 
-uint8_t NO_GUI_LEDS[] = {
-    5, //Ct_L 
-    11, //Home
-    17, //Alt_L    
-    14, //W
-    9, //A
-    15, //S
-    21, //D
-};
-
-
 static uint8_t get_current_brightness(){
      uint8_t value = rgblight_get_val();
      if(value < 64){
@@ -152,19 +137,10 @@ static uint8_t get_current_brightness(){
      return value;
 }
 
-static uint8_t get_flipped_hue(){
-    uint8_t hue = rgblight_get_hue();
-    uint8_t value = hue + 127;
-    if(value > 255){
-        return value - 255;
-    }
-    return value;
-}
-
 
 static void set_rgb_caps_leds(){
 
-     HSV hsv = {get_flipped_hue(), rgblight_get_sat(), get_current_brightness()};
+     HSV hsv = {166, 255, get_current_brightness()};
      RGB rgb = hsv_to_rgb(hsv);
     for(uint8_t i=0; i < sizeof(CAPS_LOCK_LEDS); i++ ){
         rgb_matrix_set_color(CAPS_LOCK_LEDS[i], rgb.r, rgb.g, rgb.b);
@@ -173,20 +149,10 @@ static void set_rgb_caps_leds(){
 
 static void set_rgb_scroll_lock_leds(){
 
-     HSV hsv = {get_flipped_hue(), rgblight_get_sat(), get_current_brightness()};
+     HSV hsv = {166, 255, get_current_brightness()};
      RGB rgb = hsv_to_rgb(hsv);
     for(uint8_t i=0; i < sizeof(SCROLL_LOCK_LEDS); i++ ){
         rgb_matrix_set_color(SCROLL_LOCK_LEDS[i], rgb.r, rgb.g, rgb.b);
-    }
-}
-
-
-static void set_no_gui_leds(){
-
-     HSV hsv = {get_flipped_hue(), rgblight_get_sat(), get_current_brightness()};
-     RGB rgb = hsv_to_rgb(hsv);
-    for(uint8_t i=0; i < sizeof(NO_GUI_LEDS); i++ ){
-        rgb_matrix_set_color(NO_GUI_LEDS[i], rgb.r, rgb.g, rgb.b);
     }
 }
 
@@ -201,12 +167,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
          set_rgb_scroll_lock_leds();
     }
 
-
-    if(keymap_config.no_gui){
-        set_no_gui_leds();
-    }
-
-    return false;
  // color only the leds that have an FN key pressed
  // if (get_highest_layer(layer_state) > 0) {
  //        uint8_t layer = get_highest_layer(layer_state);
@@ -223,11 +183,8 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
  //            }
  //        }
  //    }
+    return false;
 
 }
 
-
 #endif //RGB_MATRIX_ENABLE
-
-
-
